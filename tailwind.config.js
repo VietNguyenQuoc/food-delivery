94d4dd459838b1d21bdfaec6010ce49543eb0550
VietNguyenQuoc <nqviet157@gmail.com>
2022-06-06 22:36:42 +0700

    init commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#FA4A0C",
+        secondary: {
+          100: "#737373",
+          200: "#252B42",
+        },
+      },
+      fontFamily: {
+        sans: ["Montserrat"],
+        title: ["Poppins"],
+      },
+    },
+  },
+  plugins: [],
+};
